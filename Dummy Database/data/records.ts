0b2a3cbc94ab15fcc27b42caf975693376be832f
mohamedsafresh21 <104716438+mohamedsafresh21@users.noreply.github.com>
2026-02-21 15:28:47 +0530

    Create records.ts

---
+++ Dummy Database/data/records.ts
@@ -0,0 +1,5 @@
+export const records = [
+  { id: 1, owner: "user1", data: "User Record 1" },
+  { id: 2, owner: "user1", data: "User Record 2" },
+  { id: 3, owner: "admin1", data: "Admin Record" }
+];
