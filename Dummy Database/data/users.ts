97b04fcce267ba561cc385b46c6141b507bdaf80
mohamedsafresh21 <104716438+mohamedsafresh21@users.noreply.github.com>
2026-02-21 15:26:41 +0530

    Create users.ts

---
+++ Dummy Database/data/users.ts
@@ -0,0 +1,4 @@
+export const users = [
+  { id: 1, userId: "user1", password: "123", role: "GENERAL_USER", name: "John" },
+  { id: 2, userId: "admin1", password: "admin", role: "ADMIN", name: "Admin User" }
+];
